5329620a8a570d626ae086108b1bc6378eb3a71f
Christine Witt <christine.r.witt@gmail.com>
2024-02-28 21:14:42 -0600

    Update vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
   output: 'server',
   adapter: vercel({
-    webAnalytics: { enabled: true }
+    analytics: true,
   }),
 });
