1c15f4a7598d9bc900f3bd7a82aab9293a5019f7
Christine Witt <christine.r.witt@gmail.com>
2024-03-04 22:43:49 -0600

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tricountylaborclub.com',
+  integrations: [sitemap()],
   output: 'hybrid',
   adapter: vercel({
     webAnalytics: { enabled: true },
